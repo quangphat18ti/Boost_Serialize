1adc85284451525b2e329c8434d6a2389898e888
quangphat18ti <70308391+quangphat18ti@users.noreply.github.com>
2023-05-21 12:13:10 +0700

    readme.md

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 // 	g++ -I .\boostSerialize main.cpp -o main .\boostSerialize\lib\libboost_serialization-mgw63-mt-1_62.a
 
+#include "VectorSerialize.h"
+#include <boost/Serialization/vector.hpp>
 #include <fstream>
-#include <boost/archive/text_iarchive.hpp>
-#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
 #include <iostream>
 
 using namespace std;
@@ -36,37 +38,77 @@ public:
    }
 };
 
+namespace demo
+{
+   void demo()
+   {
+      const string filename = "data.bin";
+      Employee e1(11, "Harry", 4500.00f);
+      Employee e2(22, "Ravi", 8800.00f);
+      Employee e3(33, "Tim", 6800.00f);
+      Employee e4(44, "Rajiv", 3400.00f);
+
+      // Serialize and persist the object
+      {
+         std::ofstream outfile(filename, ios::binary);
+         boost::archive::binary_oarchive archive(outfile);
+         archive << e1 << e2 << e3 << e4;
+      }
+
+      // Deserialize and restore the object
+      Employee restored_e1;
+      Employee restored_e2;
+      Employee restored_e3;
+      Employee restored_e4;
+
+      {
+         std::ifstream infile(filename);
+         boost::archive::binary_iarchive archive(infile, ios::binary);
+         archive >> restored_e1 >> restored_e2 >> restored_e3 >> restored_e4;
+      }
+
+      cout << restored_e1 << endl;
+      cout << restored_e2 << endl;
+      cout << restored_e3 << endl;
+      cout << restored_e4 << endl;
+   }
+
+   // Built in VectorSerialize
+   void demoWithVector()
+   {
+      const string filename = "data.bin";
+      Employee e1(11, "Harry", 4500.00f);
+      Employee e2(22, "Ravi", 8800.00f);
+      Employee e3(33, "Tim", 6800.00f);
+      Employee e4(44, "Rajiv", 3400.00f);
+      vector<Employee> employees({e1, e2, e3, e4});
+
+      VectorSerialize<Employee> vs(employees);
+      cout << "--------- Deserialized data ------------" << endl;
+
+      string data = vs.Serialize();
+
+      VectorSerialize<Employee> vs2;
+      vs2.DeSerialize(data);
+
+      vector<Employee> restoredEmployees = vs2.rawData();
+      for (auto &e : restoredEmployees)
+         cout << e << endl
+              << endl;
+
+      cout << "------------ Writing to file ------------" << endl;
+      vs.writeToFile("dataVector.bin");
+
+      vs2.readFromFile("dataVector.bin");
+      restoredEmployees = vs2.rawData();
+      for (auto &e : restoredEmployees)
+         cout << e << endl
+              << endl;
+   }
+}
+
 int main()
 {
-   const string filename = "emp.dat";
-   Employee e1(11, "Harry", 4500.00f);
-   Employee e2(22, "Ravi", 8800.00f);
-   Employee e3(33, "Tim", 6800.00f);
-   Employee e4(44, "Rajiv", 3400.00f);
-
-   // Serialize and persist the object
-   {
-      std::ofstream outfile(filename);
-      boost::archive::text_oarchive archive(outfile);
-      archive << e1 << e2 << e3 << e4;
-   }
-
-   // Deserialize and restore the object
-   Employee restored_e1;
-   Employee restored_e2;
-   Employee restored_e3;
-   Employee restored_e4;
-
-   {
-      std::ifstream infile(filename);
-      boost::archive::text_iarchive archive(infile);
-      archive >> restored_e1 >> restored_e2 >> restored_e3 >> restored_e4;
-   }
-
-   cout << restored_e1 << endl;
-   cout << restored_e2 << endl;
-   cout << restored_e3 << endl;
-   cout << restored_e4 << endl;
-
+   demo::demoWithVector();
    return 0;
 }
